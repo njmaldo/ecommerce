12c858b9d20b9e075c05bcb43a51968e9b96ed25
nestor Maldonado <nestorjmaldo@gmail.com>
2022-11-23 15:37:00 -0300

    prueba2

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000; 
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT);
